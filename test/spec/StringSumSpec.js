516b4503e6794e64a66942ceb734da6240f7ff72
Julian Cannuli <julian.cannuli@monash.edu>
2021-07-20 11:28:22 +1000

    StringSum - Handle non-natural/negative numbers

--- test/spec/StringSumSpec.js
+++ test/spec/StringSumSpec.js
@@ -3,13 +3,19 @@ describe("StringSumCheck", function(){
     it("Two positive integers", function () {
         expect('9').toEqual(Sum('4', '5'));
         expect('53').toEqual(Sum('24', '29'));
-        expect('86').toEqual(Sum('75', '11'));
+        expect('75').toEqual(Sum('75', '0'));
     });
 
-    it("Positive and negative integer", function () {
-        expect('43').toEqual(Sum('46', '-3'));
-        expect('19').toEqual(Sum('-17', '36'));
-        expect('85').toEqual(Sum('90', '-5'));
+    it("Negative integers should be ignored", function () {
+        expect('46').toEqual(Sum('46', '-3'));
+        expect('36').toEqual(Sum('-17', '36'));
+        expect('90').toEqual(Sum('90', '-5'));
+    });
+
+    it("Invalid Non Natural Numbers should be ignored", function () {
+        expect('8').toEqual(Sum('1.6', '8'));
+        expect('37').toEqual(Sum('37', '-9.4'));
+        expect('0').toEqual(Sum('54.2', '-50.1'));
     });
 
 });
--- typefiles/stringsum.ts
+++ typefiles/stringsum.ts
@@ -1,16 +1,25 @@
 "use strict";
 /**
+ * Determines whether number is a natural number
+ * @param num
+ */
+function isNaturalNumber(num) {
+    num = num.toString();
+    var n1 = Math.abs(num), n2 = parseInt(num, 10);
+    return !isNaN(n1) && n2 === n1 && n1.toString() === num;
+}
+/**
  * Totals both string integers together
  * @param num1
  * @param num2
  */
 function Sum(num1: string, num2: string) {
 
-    if(isNaN(parseInt(num1))) {
+    if ( (isNaN(parseInt(num1))) || (!isNaturalNumber(num1)) ) {
         num1 = '0';
     }
 
-    if(isNaN(parseInt(num2))) {
+    if ( (isNaN(parseInt(num2))) || (!isNaturalNumber(num2)) ) {
         num2 = '0';
     }
 
